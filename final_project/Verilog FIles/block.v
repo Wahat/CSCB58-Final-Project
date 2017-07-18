b71e773c686975dc429f57751e5c7822aabe2448
Joon-Hong <joon.hong1998@gmail.com>
2017-07-18 15:37:52 -0400

    added block and collosion

--- final_project/Verilog FIles/block.v
+++ final_project/Verilog FIles/block.v
@@ -9,7 +9,6 @@ choose,
 
 numblocks;
 
-
 );
 
 
@@ -26,27 +25,23 @@ always@(*) begin
 	case (numblocks[3:0])
 			3'b000: begin // if block1
 					  //set vertical or horz
-					  block1[0] = choose; 
-					
+					  block1[0] = choose;
+
 			end
 			3'b001: begin // block2
-			block2[0] = choose; 
-					  
+			block2[0] = choose;
+
 			end
 			3'b010: begin // block3
-			block3[0] = choose; 
-					 
+			block3[0] = choose;
+
 			end
 			3'b011: begin // block4
-			block4[0] = choose; 
-					  
+			block4[0] = choose;
+
 			end
 	endcase
 end
-
-
-
-
 endmodule
 
 
@@ -61,13 +56,15 @@ movexright,
 moveyup,
 moveydown,
 
-set
+set,
+select,
 );
 
 input [15:0] block1;
 input [15:0] block2;
 input [15:0] block3;
 input [15:0] block4;
+input [2:0] select;
 
 input set;
 
@@ -78,5 +75,53 @@ input moveydown;
 
 reg [6:0] currentx;
 reg [6:0] currenty;
+
+always@(*) begin
+
+	if (select == 2'b00) begin
+		if (movexleft)
+			block1[1:8] = block1[1:8] - 1'b1;
+		if (movexright)
+			block1[1:8] = block1[1:8] + 1'b1;
+		if (moveyup)
+			block1[9:15] = block1[9:15] - 1'b1;
+		if (moveyup)
+			block1[9:15] = block1[9:15] + 1'b1;
+
+	end
+
+	if (select == 2'b01) begin
+		if (movexleft)
+			block2[1:8] = block2[1:8] - 1'b1;
+		if (movexright)
+			block2[1:8] = block2[1:8] + 1'b1;
+		if (moveyup)
+			block2[9:15] = block2[9:15] - 1'b1;
+		if (moveyup)
+			block2[9:15] = block2[9:15] + 1'b1;
+	end
+
+	if (select == 2'b10) begin
+		if (movexleft)
+			block3[1:8] = block3[1:8] - 1'b1;
+		if (movexright)
+			block3[1:8] = block3[1:8] + 1'b1;
+		if (moveyup)
+			block3[9:15] = block3[9:15] - 1'b1;
+		if (moveyup)
+			block3[9:15] = block3[9:15] + 1'b1;
+	end
+
+	if (select == 2'b11) begin
+		if (movexleft)
+			block4[1:8] = block4[1:8] - 1'b1;
+		if (movexright)
+			block4[1:8] = block4[1:8] + 1'b1;
+		if (moveyup)
+			block4[9:15] = block4[9:15] - 1'b1;
+		if (moveyup)
+			block4[9:15] = block4[9:15] + 1'b1;
+	end
+end
 
 endmodule
